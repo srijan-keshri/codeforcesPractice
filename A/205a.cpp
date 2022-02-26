e86e778c23e569f9026979508528e36bfbc01274
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-26 17:56:47 +0530

    9th

--- A/205a.cpp
+++ A/205a.cpp
@@ -1,3 +1,38 @@
 #include<bits/stdc++.h>
 using namespace std;
-int main()
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for (int i = 0; i < n;i++){
+        cin >> v[i];
+    }
+    int ans = v[0];
+    int mini=0;
+    for (int i = 0; i < n; i++)
+    {
+        if(v[i]<ans){
+            ans = v[i];
+            mini = i;
+        }
+    }
+    int flag = 0;
+   // cout << mini;
+    for (int i = 0; i < n; i++)
+    {
+        if(v[i]==ans && mini!=i){
+            //cout << i;
+            flag = 1;
+            break;
+        }
+    }
+   // cout << mini;
+    if (flag)
+    {
+        cout << "Still Rozdil";
+    }
+    else{
+        cout << mini+1;
+    }
+    return 0;
+}
