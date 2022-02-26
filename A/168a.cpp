c7eee9e5f97a69c0ea2367df8326436a7da23011
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-02-26 12:41:03 +0530

    6th

--- A/168a.cpp
+++ A/168a.cpp
@@ -7,6 +7,12 @@ int main(){
     no = no / 100;
     no = ceil(no);
     int ans = (no - x);
-    cout << ans;
+    if(ans>=0){
+        cout << ans;
+    }
+    else{
+        cout << "0";
+    }
+
     return 0;
 }
