2da69c65205dedcda79fb01ed3294dcac552909d
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-03-03 17:31:35 +0530

    12th

---
+++ A/118a.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string s;
+    cin >> s;
+    string ans = "";
+    for (int i = 0; i < s.length();i++){
+        if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' || s[i]=='y' || s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U' || s[i]=='Y'){
+
+        }
+        else{
+            if(isalpha(s[i])){
+                ans += '.';
+            ans += tolower(s[i]);
+            }
+        }
+    }
+    cout << ans;
+}
