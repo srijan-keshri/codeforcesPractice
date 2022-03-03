d0a73a9f4f8a52966e6ac8af1dd5b984ff51195c
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-03-03 16:34:29 +0530

    6th

--- A/320a.cpp
+++ A/320a.cpp
@@ -4,9 +4,30 @@ using namespace std;
 int main(){
     string s;
     cin >> s;
-    vector<string> v={"1","14","144"};
-    for (int i = 0; i < n;i++){
-        
+    int n = s.length();
+    int flag = 1;
+    for (int i = 0; i < n; i++)
+    {
+        if(i+2<n && s[i]=='1' && s[i+1]=='4' && s[i+2]=='4'){
+            i=i+2;
+
+        }
+        else if(i+1<n && s[i]=='1' && s[i+1]=='4'){
+++i;
+        }
+        else if(s[i]=='1'){
+
+        }
+        else{
+            flag = 0;
+            break;
+        }
+    }
+    if(flag){
+        cout << "YES";
+    }
+    else{
+        cout << "NO";
     }
 
         return 0;
