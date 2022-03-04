4344793defe291458939e79dbdd3d7efdb5713b9
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-03-04 10:18:46 +0530

    2nd

---
+++ A/233a.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin >> n;
+    if(n%2==1){
+        cout << "-1";
+    }
+    else{
+        for (int i = n ; i > 0;i--){
+            cout << i << " ";
+        }
+    }
+}
