a4190d129a66037116d3e795721b2b6f65d9767f
srijan-keshri <2019mcb1231@iitrpr.ac.in>
2022-03-12 21:23:32 +0530

    5th

--- A/92a.cpp
+++ A/92a.cpp
@@ -3,6 +3,21 @@ using namespace std;
 int main() {
     int n,m;
     cin >> n>>m;
-    
+    int index = 1;
+    while (1)
+    {
+        if(index>n){
+            index = index - n;
+        }
+        int temp =   index;
+        if(temp>m){
+            break;
+        }
+        else{
+            m -= temp;
+            index++;
+        }
+    }
+    cout << m;
     return 0;
 }
